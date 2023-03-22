106eb495cac9297b6eaada16480609787fcd0246
Yosefgeda <100337086+Yosefgeda@users.noreply.github.com>
2023-03-22 12:43:31 +0300

    Calculator UI

---
+++ src/components/Calculator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import InputField from './InputField';
+import Buttons from './Buttons';
+
+const Calculator = () => (
+  <div className="container">
+    <Buttons />
+  </div>
+);
+
+export default Calculator;
---
+++ src/components/InputField.js
@@ -0,0 +1,5 @@
+// import React from 'react';
+
+// const InputField = () => <div className="result-output">0</div>;
+
+// export default InputField;
